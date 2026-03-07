23f52ec51818f2c6c5f0ec58de4f892543c358bc
BritiJadav <britijadav@gmail.com>
2026-03-07 21:44:47 +0530

    Update __init__.py

--- src/ann/__init__.py
+++ src/ann/__init__.py
@@ -3,11 +3,9 @@ from .objective_functions import cross_entropy, mse_loss
 from .neural_layer import NeuralLayer
 from .neural_network import NeuralNetwork
 from .optimizers import (
-    stochastics_gradient_descent,
-    momentum_gradient_descent,
-    nesterov_gradient_descent,
-    rmsprop_gradient_descent,
-    adam_gradient_descent,
-    nadam_gradient_descent,
+    sgd,
+    momentum,
+    nag,
+    rmsprop
 )
 
